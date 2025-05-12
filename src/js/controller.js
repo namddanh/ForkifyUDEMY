59af3c62734bcd3cf0f98065734f28020335626f
Nam Danh <namddanh@gmail.com>
2025-05-12 15:25:14 -0400

    updating recipe servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -63,16 +63,18 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
-const controlServings = function() {
-  // Update the recipe servings (in state) 
-  model.
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
 
   // Update the recipe view
-
-}
+  const { recipe } = model.state;
+  recipeView.render(recipe);
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
