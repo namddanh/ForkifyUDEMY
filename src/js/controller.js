f4ae1cc05db4eeba243178c1da1cf349dfa31450
Nam <namddanh@gmail.com>
2025-05-14 15:37:23 -0400

    implemented preview top right for bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -21,6 +22,7 @@ const controlRecipes = async function () {
 
     // 0.75) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -77,10 +79,16 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   const { recipe } = model.state;
   if (!recipe.bookmarked) model.addBookmark(recipe);
   else model.deleteBookmark(recipe.id);
+
+  // 2) Update recipe view
   recipeView.update(recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
