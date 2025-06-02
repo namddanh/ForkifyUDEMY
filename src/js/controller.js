19dd51abda71cec77d14e98d8931c7cfd7165009
namddanh <namddanh@gmail.com>
2025-06-02 13:22:57 -0400

    cleaned controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,10 +133,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -145,7 +141,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
